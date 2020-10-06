c44d451ffad93058307f301808d4e641c0e64075
Tsegab <44075412+ziggicho@users.noreply.github.com>
2020-10-06 17:12:41 -0500

    newTrial
    dfgzdr

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		System.out.println("this is git hub");
+		
+		Trial.trial();
 	}
 
 }
---
+++ Project/src/Trial.java
@@ -0,0 +1,6 @@
+
+public class Trial {
+	public static void trial() {
+	System.out.println("trial class");
+	}
+}
